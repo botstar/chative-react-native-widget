b5b51e30992f3bdb798d0f7fc3d1a4d28f19bf78
chative-tu <tu.nguyen@chative.io>
2024-09-06 16:40:38 +0700

    feat:  🎸 implement widget state (loaded, error)

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const COLOR_WHITE = '#fff';
 export const INDEX_SHOW = 999;
 export const INDEX_HIDE = -1;
 export const WIDGET_URL = 'https://messenger.svc.chative.io/site';
+export const QUERY_URL = 'https://gateway.svc.chative.io/web/live/query';
